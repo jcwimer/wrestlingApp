f7d275724812dcd1271ce0eb5fcbd6576f0352bd
Jacob Cody Wimer <cody@wimer-dev.(none)>
2014-01-24 08:46:03 -0500

    Added seed calculation to staticpages brackets

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,6 +9,10 @@ class StaticPagesController < ApplicationController
 	    	@weight = Weight.find(params[:weight])
 	    	@wrestlers = Wrestler.where(weight_id: @weight.id)
 	    	@seed1 = Wrestler.where(weight_id: @weight.id, original_seed: 1).first
+	    	@seed10 = Wrestler.where(weight_id: @weight.id, original_seed: 10).first
+	    	@seed7 = Wrestler.where(weight_id: @weight.id, original_seed: 7).first
+	    	@seed5 = Wrestler.where(weight_id: @weight.id, original_seed: 5).first
+	    	@seed4 = Wrestler.where(weight_id: @weight.id, original_seed: 4).first
 	    end
 	    #@seed1 = @wrestlers.where(original_seed: 1).name
 
