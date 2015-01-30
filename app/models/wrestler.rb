ba8ce0b73a39e0c17974af3a15ec7e80c4176f9b
Jacob Cody Wimer <jacob.wimer@gmail.com>
2015-01-30 15:43:41 -0500

    Fixed wrestler.isWrestlingThisRound

--- app/models/wrestler.rb
+++ app/models/wrestler.rb
@@ -4,9 +4,9 @@ class Wrestler < ActiveRecord::Base
 	has_many :matches
 
 	def isWrestlingThisRound?(round)
-		@r_id = Match.where(r_id: self.id, round: round)
-		@g_id = Match.where(g_id: self.id, round: round)
-		if @r_id or @g_id
+		@r = Match.where(r_id: self.id, round: round)
+		@g = Match.where(g_id: self.id, round: round)
+		if @r or @g
 			return true
 		else
 			return false
