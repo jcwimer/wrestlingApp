662a1ad49881ce66fa2d08889eda3a8f49310af7
Jacob Cody Wimer <jacob.wimer@gmail.com>
2015-03-17 09:34:36 -0400

    Revert "Still trying to generate matches from tournament."
    This reverts commit e3a1323d39ba484909d09289795a4eef76ef6f00.

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -15,7 +15,6 @@ class Tournament < ActiveRecord::Base
 		@wrestlers = self.wrestlers
 		self.weights.sort_by{|x|[x.max]}.each do |weight|
 	    	@upcomingMatches = generateMatchups(@matches,@wrestlers,weight)
-	    	@matches = @upcomingMatches
 	    end
 	    @upcomingMatches = assignBouts(@upcomingMatches)
 	    return @upcomingMatches
