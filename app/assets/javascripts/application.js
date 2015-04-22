4185c02d77166badbd5e78952eb38eaa2ad8ff4a
Jacob Cody Wimer <jacob.wimer@gmail.com>
2015-04-21 22:11:20 -0400

    Render bootstrap and jquery from a different server.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require dataTables/jquery.dataTables
 //= require_tree .
-//= require bootstrap
 
