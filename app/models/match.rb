650a9ed7d639c5ab2b7284437e207c22f97a2866
Jacob Cody Wimer <jacob.wimer@gmail.com>
2015-04-25 13:51:16 -0400

    Moving away from matchups_array and saving matches individually

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,4 +2,13 @@ class Match < ActiveRecord::Base
 	belongs_to :tournament
 	WIN_TYPES = ["Decision", "Major", "Tech Fall", "Pin", "Forfeit", "Injury Default", "Default", "DQ"]
 
+
+	def w1_name
+		Wrestler.find(self.w1).name
+	end
+
+	def w2_name
+		Wrestler.find(self.w2).name
+	end
+
 end
