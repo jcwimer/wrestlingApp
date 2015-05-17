acb1a225dd1eb9dde72c4b695632194ad0130a2c
R.J. Osborne <rosborne@pillartechnology.com>
2015-05-17 09:49:51 -0400

    updated the db schema file to reflect the migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427163818) do
+ActiveRecord::Schema.define(version: 20150517075923) do
 
   create_table "matches", force: :cascade do |t|
     t.integer  "w1"
