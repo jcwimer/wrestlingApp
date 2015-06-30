5aecfe344b375b903b4688aa4d650e084d4c3422
jcwimer <jacob.wimer@gmail.com>
2015-06-30 18:08:30 +0000

    Trying to fix migrations

--- db/migrate/20150630170119_add_offical_weight_wrestlers.rb
+++ db/migrate/20150630170119_add_offical_weight_wrestlers.rb
@@ -2,5 +2,7 @@ class AddOfficalWeightWrestlers < ActiveRecord::Migration
   def change
     add_column :tournaments, :weigh_in_ref, :text
     add_column :wrestlers, :offical_weight, :decimal
+    add_index :tournaments, :weigh_in_ref
+    add_index :wrestlers, :offical_weight
   end
 end
--- db/migrate/20150630180257_add_index_for_weigh_ins.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexForWeighIns < ActiveRecord::Migration
-  def change
-    add_index :tournaments, :weigh_in_ref
-    add_index :wrestlers, :offical_weight
-  end
-end
