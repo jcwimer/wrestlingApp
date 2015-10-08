b449aa530bf5d747b3a598b3b3207cc798fdce68
Jacob Cody Wimer <jacob.wimer@gmail.com>
2015-10-08 17:20:00 -0400

    Added tmux window for git

--- tmux.sh
+++ tmux.sh
@@ -4,6 +4,8 @@ tmux new-session -d -s $CURRENT_SESSION
 tmux send-keys 'vim' 'C-m'
 tmux rename-window vim
 tmux new-window
-tmux rename-window server
+tmux rename-window docker
+tmux new-window
+tmux rename-window git
 tmux select-window -t 0
 tmux attach -t $CURRENT_SESSION
