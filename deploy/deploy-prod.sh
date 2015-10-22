cb1bb46451302bf842e5dac47026a047464ba6db
Jacob Cody Wimer <jacob.wimer@gmail.com>
2015-10-22 17:58:23 -0400

    Do not need sudo for compose

--- deploy/deploy-prod.sh
+++ deploy/deploy-prod.sh
@@ -2,7 +2,7 @@ gpg prod.env.gpg
 cd ..
 bash rails-prod.sh wrestlingdev
 cd deploy
-sudo docker-compose -f docker-compose-prod.yml up -d
+docker-compose -f docker-compose-prod.yml up -d
 echo Make sure your local mysql database has a db for wrestlingdev
 echo "mysqldump -u guy -ppassword -h host database_name > database.sql"
 echo "mysql -u guy -ppassword -h host database_name < database.sql"
--- deploy/deploy-test.sh
+++ deploy/deploy-test.sh
@@ -1,5 +1,5 @@
 cd ..
 bash rails-prod.sh wrestlingdev
 cd deploy
-sudo docker-compose -f docker-compose-test.yml up -d
+docker-compose -f docker-compose-test.yml up -d
 echo Make sure your local mysql database has a wrestlingtourney db
