77cda886e609d1762498537c10a4e3f93b9089c9
jcwimer <jacob.wimer@gmail.com>
2015-11-05 16:55:27 +0000

    Changes in cloud 9

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20151023123932) do
 
   add_index "matches", ["mat_id"], name: "index_matches_on_mat_id"
   add_index "matches", ["tournament_id"], name: "index_matches_on_tournament_id"
-  add_index "matches", ["w1", "w2"], name: "index_matches_on_w1_and_w2"
+  add_index "matches", ["w1", "w2"], name: "index_matches_on_w1_and_w2", unique: true
 
   create_table "mats", force: :cascade do |t|
     t.string   "name"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20151023123932) do
   end
 
   add_index "tournaments", ["user_id"], name: "index_tournaments_on_user_id"
+  add_index "tournaments", ["weigh_in_ref"], name: "index_tournaments_on_weigh_in_ref"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -113,6 +114,7 @@ ActiveRecord::Schema.define(version: 20151023123932) do
     t.decimal  "offical_weight"
   end
 
+  add_index "wrestlers", ["offical_weight"], name: "index_wrestlers_on_offical_weight"
   add_index "wrestlers", ["weight_id"], name: "index_wrestlers_on_weight_id"
 
 end
