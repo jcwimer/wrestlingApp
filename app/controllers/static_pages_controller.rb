0e5229f2a3c05ca2afa244cd4455b7257c41e4f4
jcwimer <jacob.wimer@gmail.com>
2015-11-20 12:41:11 +0000

    Created an about page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,18 +1,11 @@
 class StaticPagesController < ApplicationController
-  before_filter :check_access, only: [:weigh_in]
 
 
 
 	def not_allowed
 	end
 
-	private
-	def check_access
-	  if params[:tournament]
-	     @tournament = Tournament.find(params[:tournament])
-	     if current_user != @tournament.user
-		redirect_to '/static_pages/not_allowed'
-	     end	
-	  end
+	def about
+		
 	end
 end
