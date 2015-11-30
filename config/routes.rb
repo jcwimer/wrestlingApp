ce2e80a2b43dd67ca6773fd1802370af87ba0929
jcwimer <jacob.wimer@gmail.com>
2015-11-30 15:18:03 +0000

    Created about page

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Wrestling::Application.routes.draw do
   get 'admin/index'
   get 'static_pages/control_match'
   get 'static_pages/not_allowed'
+  get 'static_pages/about'
 
   get 'tournaments/:id/weigh_in/:weight'  => 'tournaments#weigh_in_weight'
   post 'tournaments/:id/weigh_in/:weight'  => 'tournaments#weigh_in_weight'
