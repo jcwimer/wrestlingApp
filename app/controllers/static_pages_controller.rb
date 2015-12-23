49bcbfcc8459f91fa02e2ad87248897d76a68a2a
jcwimer <jacob.wimer@gmail.com>
2015-12-23 15:33:27 +0000

    Order my_tournaments by updated_at

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
 
 	def my_tournaments
-		@tournaments = current_user.tournaments
+		@tournaments = current_user.tournaments.order('updated_at desc')
 	end
 
 	def not_allowed
