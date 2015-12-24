c9eed5ef2b245e226b0574380f702c6e8d9b60d0
Jacob Cody Wimer <jacob.wimer@gmail.com>
2015-12-24 08:09:09 -0500

    Updated rails-prod.sh

--- rails-prod.sh
+++ rails-prod.sh
@@ -6,4 +6,4 @@ if [ $# != 1 ]; then
 fi
 
 docker build -t $1 -f rails-prod-Dockerfile .
-#sudo docker run -d --restart=always -p 80:80 -p 443:443 $1
+docker run -d --restart=always --env-file $WRESTLINGDEV_ENV_FILE -p 80:80 -p 443:443 $1
