c834985c5d4f4546280fb9e299713941da70cdd4
jcwimer <jacob.wimer@gmail.com>
2016-01-04 16:49:31 +0000

    Trying to fix email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,9 @@ Wrestling::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  
+  #Devise needs origin of email
+  config.action_mailer.default_url_options = { :host => 'wrestlingapp.heroku.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
