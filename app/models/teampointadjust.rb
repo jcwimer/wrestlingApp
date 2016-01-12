faa4300335bc7e4df136902b4ab17c7156b8ae46
jcwimer <jacob.wimer@gmail.com>
2016-01-12 18:41:43 +0000

    Touch wrestler and school on update

--- app/models/teampointadjust.rb
+++ app/models/teampointadjust.rb
@@ -1,6 +1,6 @@
 class Teampointadjust < ActiveRecord::Base
-    belongs_to :wrestler
-    belongs_to :school
+    belongs_to :wrestler, touch: true
+    belongs_to :school, touch: true
     
     after_save do
         advance_wrestlers_and_calc_team_score
