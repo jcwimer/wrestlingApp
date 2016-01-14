596964b7acf594325f85906014204e44981bfc22
jcwimer <jacob.wimer@gmail.com>
2016-01-14 19:27:32 +0000

    Added worker script

---
+++ rails-worker-prod.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+if [ $# != 1 ]; then
+  echo "Please enter docker image name for the rails development environment"
+  exit 1
+fi
+
+docker build -t $1 -f rails-prod-Dockerfile .
+docker run -d --restart=always --env-file $WRESTLINGDEV_ENV_FILE $1 rake db:work
