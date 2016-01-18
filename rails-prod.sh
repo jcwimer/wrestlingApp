b71e5bdd787c653752c018e5a746b888532c5125
jcwimer <jacob.wimer@gmail.com>
2016-01-18 16:23:59 +0000

    Kill does not need to happen before build

--- rails-prod.sh
+++ rails-prod.sh
@@ -4,6 +4,7 @@ if [ $# != 1 ]; then
   echo "Please enter docker image name for the rails development environment"
   exit 1
 fi
-docker kill $1
+
 docker build -t $1 -f rails-prod-Dockerfile .
+docker kill $1
 docker run --name $1 -d --restart=always --env-file $WRESTLINGDEV_ENV_FILE -p 80:80 -p 443:443 $1
--- rails-worker-prod.sh
+++ rails-worker-prod.sh
@@ -4,6 +4,7 @@ if [ $# != 1 ]; then
   echo "Please enter docker image name for the rails development environment"
   exit 1
 fi
-docker kill $1
+
 docker build -t $1 -f rails-prod-Dockerfile .
+docker kill $1
 docker run --name $1 -d --restart=always --env-file $WRESTLINGDEV_ENV_FILE $1 bundle exec rake jobs:work RAILS_ENV=production
