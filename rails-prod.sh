3ae5727f09b45396507b68e45d6c69a9eee30074
jcwimer <jacob.wimer@gmail.com>
2016-01-28 16:37:04 +0000

    Set hostname for production docker containers

--- rails-prod.sh
+++ rails-prod.sh
@@ -9,4 +9,4 @@ docker kill $1
 docker rm $1
 docker build -t $1 -f rails-prod-Dockerfile .
 
-docker run --name $1 -d --restart=always --env-file $WRESTLINGDEV_ENV_FILE -v /srv/docker/apache2/logs:/var/log/apache2  -v /etc/localtime:/etc/localtime -p 80:80 -p 443:443 $1
+docker run -h $HOSTNAME --name $1 -d --restart=always --env-file $WRESTLINGDEV_ENV_FILE -v /srv/docker/apache2/logs:/var/log/apache2  -v /etc/localtime:/etc/localtime -p 80:80 -p 443:443 $1
