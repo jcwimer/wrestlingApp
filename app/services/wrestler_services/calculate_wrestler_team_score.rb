ba055739280471827ac3d43cfda0344353c69bee
Jacob Cody Wimer <jacob.wimer@gmail.com>
2017-12-14 09:03:31 -0500

    Pretty up wrestler show page

--- app/services/wrestler_services/calculate_wrestler_team_score.rb
+++ app/services/wrestler_services/calculate_wrestler_team_score.rb
@@ -3,19 +3,19 @@ class CalculateWrestlerTeamScore
       @wrestler = wrestler
       @tournament = @wrestler.tournament
     end
-    
+
     def totalScore
         if @wrestler.extra or @wrestler.matches.count == 0
-			return 0            
+			return 0
         else
             earnedPoints - deductedPoints
         end
     end
-    
+
     def earnedPoints
-        return poolPoints + bracketPoints + placementPoints + bonusWinPoints
+        return poolPoints + bracketPoints + placementPoints + bonusWinPoints + byePoints
     end
-    
+
     def deductedPoints
         points = 0
 		@wrestler.deductedPoints.each do |d|
@@ -23,29 +23,37 @@ class CalculateWrestlerTeamScore
 		end
 		points
     end
-    
+
     def placementPoints
         PoolBracketPlacementPoints.new(@wrestler).calcPoints if @tournament.tournament_type == "Pool to bracket"
     end
-    
+
     def bracketPoints
         (@wrestler.championshipAdvancementWins.size * 2) + (@wrestler.consoAdvancementWins.size * 1)
     end
-    
+
     def poolPoints
         if @tournament.tournament_type == "Pool to bracket"
-            if @wrestler.poolWins.size >= 1 and @wrestler.hasAPoolBye == true
-                ((@wrestler.poolWins.size * 2) + 2)
-            else
-                (@wrestler.poolWins.size * 2)
-            end
+          (@wrestler.poolWins.size * 2)
         else
-            0
+          0
         end
     end
-    
-    def bonusWinPoints
-        (@wrestler.pinWins.size * 2) + (@wrestler.techWins.size * 1.5) + (@wrestler.majorWins.size * 1) 
+
+    def byePoints
+        if @tournament.tournament_type == "Pool to bracket"
+            if @wrestler.poolWins.size >= 1 and @wrestler.hasAPoolBye == true
+              2
+            else
+              0
+            end
+        else
+          0
+        end
     end
-    
+
+    def bonusWinPoints
+        (@wrestler.pinWins.size * 2) + (@wrestler.techWins.size * 1.5) + (@wrestler.majorWins.size * 1)
+    end
+
 end
