ebbc92e0c0933097cc01219c5d987fc869a1ef0f
Jacob Cody Wimer <jacob.wimer@gmail.com>
2018-01-31 09:15:48 -0500

    Only assign matches with about number

--- app/models/mat.rb
+++ app/models/mat.rb
@@ -1,36 +1,36 @@
 class Mat < ActiveRecord::Base
 	belongs_to :tournament
 	has_many :matches
-	
+
 	validates :name, presence: true
-	
-	before_destroy do 
+
+	before_destroy do
 		if tournament.matches.size > 0
 			tournament.resetMats
 			matsToAssign = tournament.mats.select{|m| m.id != self.id}
 			tournament.assignMats(matsToAssign)
 		end
 	end
-	
-	after_create do 
+
+	after_create do
 		if tournament.matches.size > 0
 			tournament.resetMats
 			matsToAssign = tournament.mats
 			tournament.assignMats(matsToAssign)
 		end
 	end
-	
+
 	def assignNextMatch
-		t_matches = tournament.matches.select{|m| m.mat_id == nil && m.finished != 1}
+		t_matches = tournament.matches.select{|m| m.mat_id == nil && m.finished != 1 && bout_number != nil}
 		if t_matches.size > 0
 			match = t_matches.sort_by{|m| m.bout_number}.first
 			match.mat_id = self.id
 			match.save
 		end
 	end
-	
+
 	def unfinishedMatches
 		matches.select{|m| m.finished != 1}.sort_by{|m| m.bout_number}
 	end
-	
+
 end
