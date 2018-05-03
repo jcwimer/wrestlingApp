6aeb64538f593279db41177b522e005180d7a761
Jacob Cody Wimer <jacob.wimer@gmail.com>
2018-05-03 18:45:25 +0000

    Updated to Ruby 2.5.1 and Rails 5.2.0

--- bin/run-tests-with-docker.sh
+++ bin/run-tests-with-docker.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 project_dir="$(dirname $( dirname $(readlink -f ${BASH_SOURCE[0]})))"
 
 docker build -f ${project_dir}/deploy/rails-prod-Dockerfile -t wrestlingdevtests ${project_dir}/.
