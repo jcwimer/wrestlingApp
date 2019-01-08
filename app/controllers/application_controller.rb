2a506e072d920dd000806dc8e9a520cb96ff5ed7
Jacob Cody Wimer <jacob.wimer@gmail.com>
2019-01-08 16:02:25 +0000

    Devise needs to prepend forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  	
+  protect_from_forgery prepend: true, with: :exception
+
   after_action :set_csrf_cookie_for_ng
   
   def set_csrf_cookie_for_ng
