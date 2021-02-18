5677f4e9441000cbf3ba392a70ed629feeed8fae
Jacob Cody Wimer <jacob.wimer@gmail.com>
2021-02-18 20:27:44 +0000

    Updated development to allow all hostnames (for localtunnel)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 Wrestling::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # allow all hostnames
+	config.hosts.clear
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -32,9 +35,5 @@ Wrestling::Application.configure do
 	#Bullet.alert = true
 	#Bullet.console = true
 	#Bullet.bullet_logger = true
-	
-	
-
-
   end
 end
