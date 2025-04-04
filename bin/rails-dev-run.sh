e30f8fa83d9210d17b6a1c9c64332d3eb65d83fd
Jacob Cody Wimer <cody.wimer@innovaccer.com>
2025-04-03 22:49:10 -0400

    bin/rails-dev-run.sh no longer needs to chmod after exiting

--- bin/rails-dev-run.sh
+++ bin/rails-dev-run.sh
@@ -2,14 +2,19 @@
 project_dir="$(dirname $( dirname $(readlink -f ${BASH_SOURCE[0]})))"
 
 USER_ID=$(id -u ${USER})
-# Get group id for username
-GROUP_ID=$(cut -d: -f3 < <(getent group ${USER}))
+# Get group id for username - fixed to correctly retrieve numeric GID
+GROUP_ID=$(id -g ${USER})
 
 if [ $# != 1 ]; then
   echo "Please enter docker image name for the rails development environment"
   exit 1
 fi
 
-docker build -t $1 -f ${project_dir}/deploy/rails-dev-Dockerfile ${project_dir}
-docker run --rm -it -p 3000:3000 -v ${project_dir}:/rails $1 /bin/bash
-sudo chown -R ${USER_ID}:${USER_ID} ${project_dir}
+docker build -t $1 -f ${project_dir}/deploy/rails-dev-Dockerfile \
+  --build-arg USER_ID=$USER_ID \
+  --build-arg GROUP_ID=$GROUP_ID \
+  ${project_dir}
+
+docker run --rm -it -p 3000:3000 \
+  -v ${project_dir}:/rails \
+  $1 /bin/bash
