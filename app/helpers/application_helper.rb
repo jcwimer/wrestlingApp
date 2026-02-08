07d43e77207695ce44bea0f6e787848b21a7fc0d
Jacob Cody Wimer <jacob.wimer@gmail.com>
2026-02-08 18:59:42 -0500

    Hide ads for coaches when submitting lineups

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
-  
+  def hide_ads?
+    return false unless controller_name == "schools"
+    return false unless %w[show edit new].include?(action_name)
 
+    user_signed_in? || school_permission_key_present?
+  end
+
+  def school_permission_key_present?
+    @school_permission_key.present? ||
+      params[:school_permission_key].present? ||
+      params.dig(:school, :school_permission_key).present?
+  end
 end
